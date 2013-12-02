69bd0a8782f405e86a65efbb14dd93722671ccbb
Alkxzv <alkxzo@gmail.com>
2013-12-03 00:48:57 +0100

    Soybean loader. Working on the dataset generation.

--- experiments/synthetic.py
+++ experiments/synthetic.py
@@ -3,6 +3,7 @@
 import json
 import os
 import sys
+import time
 
 # FIXME: Quick path hack.
 sys.path.insert(0, os.path.abspath('.'))
@@ -14,15 +15,33 @@ from sklearn import svm
 
 from kcat.datasets import synthetic
 from kcat.kernels.categorical import get_pgen, fast_k0, fast_k1, fast_k2
-from kcat.misc import preprocess, GridSearchK0, GridSearchK1
+from kcat.misc import GridSearchK0, GridSearchK1
 
-# Generate a dataset and split the data in train and test
-X, y = synthetic(320, d=25, c=4)
 
-X_train, X_test, y_train, y_test = cv.train_test_split(X, y, test_size=0.5, random_state=42)
+# Load data:
+f = open("./experiments/results/synthetic.json", "r")
+data = json.load(f)
+f.close()
+
+
+# Find next available ID:
+lastid = -1
+for d in data:
+    lastid = d['meta']['id'] if lastid < d['meta']['id'] else lastid
+nextid = lastid + 1
+
+
+# Generate a dataset:
+data = dict(m=320, n=25, c=4, p=0.5, seed=nextid, encoder=True)
+X, y, benc = synthetic(**data)
+# Split the data in train and test:
+split = cv.train_test_split(X, y, test_size=0.5, random_state=nextid)
+X_train, X_test, y_train, y_test = split
 X_train_pgen = get_pgen(X_train)
-X_train_prep =  preprocess(X_train)
-X_test_prep = preprocess(X_test)
+X_train_prep =  benc(X_train)
+X_test_prep = benc(X_test)
+# Specify the cross-validation to use
+
 
 
 # Fit
@@ -33,7 +52,7 @@ costs = 10.0 ** np.arange(-1, 5)
 gammas = 2.0 ** np.arange(-12, -4)
 params = dict(C=costs, gamma=gammas)
 
-grid = gs.GridSearchCV(clf, param_grid=params, cv=10)
+grid = gs.GridSearchCV(clf, param_grid=params, cv=10, random_state=nextid)
 grid.fit(X_train_prep, y_train)
 
 rbf_fit = grid.best_params_
@@ -52,41 +71,41 @@ functions = [
 ]
 gammas = 2.0 ** np.arange(-3, 2)
 
-grid = GridSearchK0(functions, gammas, param_grid=dict(C=costs), cv=10)
+grid = GridSearchK0(functions, gammas, param_grid=dict(C=costs), cv=10, random_state=nextid)
 grid = grid.fit(X_train, y_train)
 
 k0_fit = grid.best_params_
 
-# # K1
-# clf = svm.SVC(kernel='precomputed')
-# costs = 10.0 ** np.arange(-1, 5)
-# functions = [
-#     ('ident', 'mean', 'ident'),
-#     ('ident', 'mean', 'f1'),
-#     ('ident', 'mean', 'f2'),
-#     ('ident', 'prod', 'ident'),
-#     ('ident', 'prod', 'f1'),
-#     ('ident', 'prod', 'f2'),
-#     ('f1', 'mean', 'ident'),
-#     ('f1', 'prod', 'ident'),
-# ]
-# gammas = 2.0 ** np.arange(-3, 2)
-# alphas = 2.0 ** np.arange(-1, 2)
+# K1
+clf = svm.SVC(kernel='precomputed')
+costs = 10.0 ** np.arange(-1, 5)
+functions = [
+    ('ident', 'mean', 'ident'),
+    ('ident', 'mean', 'f1'),
+    ('ident', 'mean', 'f2'),
+    ('ident', 'prod', 'ident'),
+    ('ident', 'prod', 'f1'),
+    ('ident', 'prod', 'f2'),
+    ('f1', 'mean', 'ident'),
+    ('f1', 'prod', 'ident'),
+]
+gammas = 2.0 ** np.arange(-3, 2)
+alphas = 2.0 ** np.arange(-1, 2)
 
-# grid = GridSearchK1(alphas, functions, gammas, param_grid=dict(C=costs), cv=10)
-# grid = grid.fit(X_train, X_train_pgen, y_train)
+grid = GridSearchK1(alphas, functions, gammas, param_grid=dict(C=costs), cv=10, random_state=nextid)
+grid = grid.fit(X_train, X_train_pgen, y_train)
 
-# k1_fit = grid.best_params_
+k1_fit = grid.best_params_
 
-# # K2
-# clf = svm.SVC(kernel='precomputed')
-# costs = 10.0 ** np.arange(-1, 4)
-# param_grid = dict(C=costs)
-# grid = gs.GridSearchCV(clf, param_grid=param_grid, cv=10)
-# gram = fast_k2(X_train, X_train, X_train_pgen)
-# grid.fit(gram, y_train)
+# K2
+clf = svm.SVC(kernel='precomputed')
+costs = 10.0 ** np.arange(-1, 4)
+param_grid = dict(C=costs)
+grid = gs.GridSearchCV(clf, param_grid=param_grid, cv=10, random_state=nextid)
+gram = fast_k2(X_train, X_train, X_train_pgen)
+grid.fit(gram, y_train)
 
-# k2_fit = grid.best_params_
+k2_fit = grid.best_params_
 
 # Evaluation
 
@@ -107,27 +126,25 @@ y_predict = clf.predict(fast_k0(X_test, X_train, **rb))
 
 results['k0'] = (y_predict == y_test).mean()
 
-# # K1
-# ra, rb = k1_fit
-# clf = svm.SVC(kernel='precomputed', **ra)
-# clf.fit(fast_k1(X_train, X_train, X_train_pgen, **rb), y_train)
-# y_predict = clf.predict(fast_k1(X_test, X_train, X_train_pgen, **rb))
+# K1
+ra, rb = k1_fit
+clf = svm.SVC(kernel='precomputed', **ra)
+clf.fit(fast_k1(X_train, X_train, X_train_pgen, **rb), y_train)
+y_predict = clf.predict(fast_k1(X_test, X_train, X_train_pgen, **rb))
 
-# results['k1'] = (y_predict == y_test).mean()
+results['k1'] = (y_predict == y_test).mean()
 
-# # K2
-# clf = svm.SVC(kernel='precomputed', C=10.0)
-# clf.fit(fast_k2(X_train, X_train, X_train_pgen), y_train)
-# y_predict = clf.predict(fast_k2(X_test, X_train, X_train_pgen))
+# K2
+clf = svm.SVC(kernel='precomputed', C=10.0)
+clf.fit(fast_k2(X_train, X_train, X_train_pgen), y_train)
+y_predict = clf.predict(fast_k2(X_test, X_train, X_train_pgen))
 
-# results['k2'] = (y_predict == y_test).mean()
+results['k2'] = (y_predict == y_test).mean()
 
 
 # Update file
 
-f = open("./experiments/results/synthetic.json", "r")
-data = json.load(f)
-f.close()
+results['meta'] = {'id': nextid, 'data': data, 'datetime': time.asctime()}
 
 data.append(results)
 
--- kcat/datasets.py
+++ kcat/datasets.py
@@ -1,44 +1,83 @@
-import random
 import urllib.request
+import collections
 
 import numpy as np
+from numpy import random
 
 
-def synthetic(n, d=20, c=2, p=0.5):
+def synthetic(m, n=25, c=4, p=0.5, encoder=False, random_state=None):
     """
-    * `n`: number of examples to generate.
-    * `d`: number of attributes for each example.
-    * `c`: number of classes.
-    * `p`: change probability of class-unique attributes:
-        * `p` close to 0: class attributes almost never happen (random/harder).
-        * `p` close to 1: class attributes happen as often as the rest (easier).
+    :param m: Number of examples to generate.
+    :param n: Number of attributes for each example.
+    :param c: Number of classes.
+    :param p: Change probability of class-unique attributes.
+    :type p: float
+
+    The effect of the parameter `p` according to its value:
+
+    - *p* close to 0: Class attributes almost never happen (random/harder).
+    - *p* close to 1: Class attributes happen as often as the rest (easier).
+
+    :returns: - A matrix of size :math:`m \\times n` with the dataset.
+              - An array with the class of the `m` examples.
+              - (optional) A binary encoding function for X.
     """
+    random.seed(random_state)
     p **= 2.0  # Makes the effect of the parameter more linear.
     a = c + 2  # Number of attribute values.
     # Assign class to each example:
-    y = [i % c for i in range(n)]
+        # y = random.randint(c, size=m)
+    k = [float(i % c) for i in range(m)]
+    y = random.choice(k, m, replace=False)
     # Generate attributes:
-    x = [[] for _ in range(n)]
-    for i in range(d):
+    X = np.zeros((m, n))
+    for j in range(n):
         # Generate a list of values:
-        common = list(range(a))
+        values = np.arange(a)
         # Pick one value at random for each class:
-        unique = [common.pop(random.randrange(len(common))) for _ in range(c)]
+        unique = random.choice(values, c, replace=False)
+        # The rest are common attributes:
+        common = list(set(values) - set(unique))
         # Generate attributes:
-        for j in range(n):
+        for i in range(m):
             # Choose to assing a random attribute or a unique one:
-            rand = random.random() > c / a * p
-            value = random.choice(common) if rand else unique[y[j]]
-            x[j].append(value)
-    return np.array(x), np.array(y)
+            rand = random.random() > (c / a * p)
+            value = random.choice(common) if rand else unique[y[i]]
+            X[i][j] = value
+    # When encoder is True, return a binary encoder for the dataset. The
+    # encoder is necessary because small datasets may not contain instances of
+    # every single category in the dataset, making it impossible to infer the
+    # correct binarization from just part of the whole dataset.
+    if not encoder:
+        return X, y
+    else:
+        def binary_encoder(X):
+            # Each attribute has a value between 0 and `a`.
+            # We create the matrix with zeros, with a column for each possible
+            # category of each attribute, then set to one the corresponding
+            # column for each example.
+            Y = np.zeros((m, n * a))
+            # For each attribute:
+            for i in range(m):
+                for j in range(n):
+                    v = X[i][j]
+                    Y[i][j * a + v] = 1
+            return Y
+        return X, y, binary_encoder
 
 
-def gmonks(n, d):
+def gmonks(m, d=3, random_state=None):
     """
-    Returns a matrix with `n` examples of the monks dataset as rows, which have
-    `d` * 6 features each. Also returns a vector of `n` booleans with the class
-    of each example.
+    :param m: Number of examples to generate.
+    :param d: Number of blocks of features for each example.
+
+    Each block is a set of six features generated according to the description
+    in the original monks problem.
+
+    :returns: - A matrix of size :math:`m \\times (6d)` with the dataset.
+              - An array with the class of the `m` examples.
     """
+    random.seed(random_state)
     # Give names to the categories, makes the code easier to read:
     C1, C2, C3, C4 = range(4)
     categories = (
@@ -50,7 +89,7 @@ def gmonks(n, d):
         (C1, C2),
     )
     x, y = [], []
-    for i in range(n):
+    for i in range(m):
         x.append([])
         fk = 0
         for j in range(d):
@@ -64,15 +103,65 @@ def gmonks(n, d):
     return np.array(x), np.array(y)
 
 
-def promoters():
- with urllib.request.urlopen(
-    'http://archive.ics.uci.edu/ml/machine-learning-databases/'
-    'molecular-biology/promoter-gene-sequences/promoters.data'
-) as promoters_data:
-    categories = {'a': 0, 'c': 1, 'g': 2, 't': 3}
-    x, y = [], []
-    for line in promoters_data:
-        cat, _, seq = line.decode('ascii').split(',')
-        x.append([categories[i] for i in seq.strip()])
-        y.append(cat == '+')
-    return np.array(x), np.array(y)
+def promoters(random_state=None):
+    """
+    Downloads the promoter gene sequences dataset from the internet and loads
+    them into a data set.
+
+    :returns: - A matrix of size :math:`106 \\times 57` with the dataset.
+              - An array with the class of the `106` examples.
+    """
+    # TODO: Add shuffle.
+    random.seed(random_state)
+    with urllib.request.urlopen(
+        'http://archive.ics.uci.edu/ml/machine-learning-databases/'
+        'molecular-biology/promoter-gene-sequences/promoters.data'
+    ) as promoters_data:
+        categories = {'a': 0, 'c': 1, 'g': 2, 't': 3}
+        X, y = [], []
+        for line in promoters_data:
+            cat, _, seq = line.decode('ascii').split(',')
+            X.append([categories[i] for i in seq.strip()])
+            y.append(cat == '+')
+        X, y = np.array(X), np.array(y)
+        return X,  y
+
+
+def soybean(random_state=None):
+    """
+    Downloads the soybean dataset from the internet.
+
+    :returns: - A matrix of size :math:`? \\times ?` with the dataset.
+              - An array with the class of the `?` examples.
+    """
+    # TODO: Add shuffle.
+    random.seed(random_state)
+    with urllib.request.urlopen(
+        'http://archive.ics.uci.edu/ml/machine-learning-databases/'
+        'soybean/soybean-large.data'
+    ) as promoters_data:
+        X, y = [], []
+        encode = lambda x: 0 if x == '?' else int(x) + 1
+        for line in promoters_data:
+            seq = line.decode('ascii').split(',')
+            y.append(seq[0])
+            X.append([encode(x.strip()) for x in seq[2:]])
+        X, y = np.array(X), np.array(y)
+        return X,  y
+
+
+def soybean_test(random_state=None):
+    # TODO: Add shuffle.
+    random.seed(random_state)
+    with urllib.request.urlopen(
+        'http://archive.ics.uci.edu/ml/machine-learning-databases/'
+        'soybean/soybean-large.test'
+    ) as promoters_data:
+        X, y = [], []
+        encode = lambda x: 0 if x == '?' else int(x) + 1
+        for line in promoters_data:
+            seq = line.decode('ascii').split(',')
+            y.append(seq[0])
+            X.append([encode(x.strip()) for x in seq[2:]])
+        X, y = np.array(X), np.array(y)
+        return X,  y
